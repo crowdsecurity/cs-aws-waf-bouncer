d367e573cc0d3e4822202bba7e31e86eb513a9de
marco <marco@crowdsec.net>
2024-03-19 23:35:35 +0100

    lint

--- pkg/waf/ipset_manager.go
+++ pkg/waf/ipset_manager.go
@@ -22,6 +22,7 @@ func (im *IPSetManager) FindAvailableSet(ipType string, decisionType string) (*W
 			return ipset, nil
 		}
 	}
+
 	return nil, fmt.Errorf("no available set found")
 }
 
@@ -31,20 +32,24 @@ func (im *IPSetManager) alreadyInSets(ip string, decisionType string) bool {
 			return true
 		}
 	}
+
 	return false
 }
 
 func (im *IPSetManager) Commit() error {
 	for _, ipset := range im.IPSets {
 		im.logger.Debugf("checking if set %s is stale", ipset.GetName())
+
 		if ipset.IsStale() {
 			im.logger.Infof("set %s is stale, updating it", ipset.GetName())
+
 			err := ipset.Commit()
 			if err != nil {
 				return err
 			}
 		}
 	}
+
 	return nil
 }
 
@@ -67,8 +72,10 @@ func (im *IPSetManager) AddIp(ip string, decisionType string) {
 		ipset = NewIpSet(im.setPrefix, ipType, decisionType, im.scope, im.client)
 		im.IPSets = append(im.IPSets, ipset)
 		ipset.Add(ip)
+
 		return
 	}
+
 	ipset.Add(ip)
 }
 
@@ -83,6 +90,7 @@ func (im *IPSetManager) DeleteIp(ip string, decisionType string) {
 
 func (im *IPSetManager) DeleteEmptySets() {
 	removed := make([]*WAFIpSet, 0)
+
 	for _, ipset := range im.IPSets {
 		if ipset.Size() == 0 {
 			removed = append(removed, ipset)
@@ -91,10 +99,12 @@ func (im *IPSetManager) DeleteEmptySets() {
 
 	for _, ipset := range removed {
 		im.logger.Infof("set %s is empty, deleting it.", ipset.GetName())
+
 		err := ipset.DeleteIpSet()
 		if err != nil {
 			im.logger.Errorf("could not delete set %s: %s", ipset.GetName(), err)
 		}
+
 		im.IPSets = removeIpSetFromSlice(im.IPSets, ipset)
 	}
 }
@@ -102,6 +112,7 @@ func (im *IPSetManager) DeleteEmptySets() {
 func (im *IPSetManager) DeleteSets() {
 	for _, ipset := range im.IPSets {
 		im.logger.Infof("deleting set %s", ipset.GetName())
+
 		err := ipset.DeleteIpSet()
 		if err != nil {
 			im.logger.Errorf("could not delete set %s: %s", ipset.GetName(), err)
@@ -111,6 +122,7 @@ func (im *IPSetManager) DeleteSets() {
 
 func NewIPSetManager(setPrefix string, scope string, client *wafv2.WAFV2, logger *log.Entry) *IPSetManager {
 	l := logger.WithField("component", "ipset_manager")
+
 	return &IPSetManager{
 		IPSets:    make([]*WAFIpSet, 0),
 		setPrefix: setPrefix,
