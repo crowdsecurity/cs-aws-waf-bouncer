33c41eaf6fe656c5cebb2f9e722776d0cfde0589
Sebastien Blot <sebastien@crowdsec.net>
2025-07-23 21:36:46 +0200

    up

--- test/tests/bouncer/test_aws_waf_bouncer.py
+++ test/tests/bouncer/test_aws_waf_bouncer.py
@@ -1,19 +1,22 @@
-
 def test_no_api_key(crowdsec, bouncer, aw_cfg_factory):
     cfg = aw_cfg_factory()
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: api_key or certificates paths are required*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: api_key or certificates paths are required*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
-    cfg['api_key'] = ''
+    cfg["api_key"] = ""
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: api_key or certificates paths are required*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: api_key or certificates paths are required*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
@@ -21,92 +24,108 @@ def test_no_api_key(crowdsec, bouncer, aw_cfg_factory):
 def test_no_lapi_url(bouncer, aw_cfg_factory):
     cfg = aw_cfg_factory()
 
-    cfg['api_key'] = 'not-used'
+    cfg["api_key"] = "not-used"
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: api_url is required*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: api_url is required*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
-    cfg['api_url'] = ''
+    cfg["api_url"] = ""
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: api_url is required*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: api_url is required*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
 
 def test_partial_config(bouncer, aw_cfg_factory):
     cfg = aw_cfg_factory()
-    cfg['api_key'] = 'not-used'
-    cfg['api_url'] = 'http://localhost:8237'
+    cfg["api_key"] = "not-used"
+    cfg["api_url"] = "http://localhost:8237"
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: waf_config is required*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: waf_config is required*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
-    waf = {
-        'web_acl_name': 'testwebacl'
-    }
+    waf = {"web_acl_name": "testwebacl"}
 
-    cfg['waf_config'] = [waf]
+    cfg["waf_config"] = [waf]
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: fallback_action is required*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: fallback_action is required*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
-    waf['fallback_action'] = 'ban'
+    waf["fallback_action"] = "ban"
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: rule_group_name is required*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: rule_group_name is required*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
-    waf['rule_group_name'] = 'crowdsec-rule-group-eu-west-1'
+    waf["rule_group_name"] = "crowdsec-rule-group-eu-west-1"
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: scope is required*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: scope is required*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
-    waf['scope'] = 'REGIONAL'
+    waf["scope"] = "REGIONAL"
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: ipset_prefix is required*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: ipset_prefix is required*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
-    waf['ipset_prefix'] = 'crowdsec-ipset-a'
+    waf["ipset_prefix"] = "crowdsec-ipset-a"
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not parse configuration: region is required when scope is REGIONAL*",
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not parse configuration: region is required when scope is REGIONAL*",
+            ]
+        )
         aw.proc.wait(timeout=0.2)
         assert not aw.proc.is_running()
 
-    waf['region'] = 'eu-west-1'
+    waf["region"] = "eu-west-1"
 
     with bouncer(cfg) as aw:
-        aw.wait_for_lines_fnmatch([
-            "*could not initialize waf instance: failed to list resources: NoCredentialProviders: no valid providers in chain*"
-        ])
+        aw.wait_for_lines_fnmatch(
+            [
+                "*could not initialize waf instance: failed to list resources: operation error WAFV2: ListWebACLs, get identity: get credentials: failed to refresh cached credentials, no EC2 IMDS role found*"
+            ]
+        )
 
         # this requires more time
         aw.proc.wait(timeout=5)
